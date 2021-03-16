5d6b862537ec11a4497f335077f6c4333fb2b7df
Connorsmith25 <71672667+Connorsmith25@users.noreply.github.com>
2021-03-16 16:46:40 -0400

    Add files via upload

--- HW05_Amocking.py
+++ HW05_Amocking.py
@@ -1,13 +1,35 @@
 import unittest
-import HW04a
-from HW04a import github_repos_commits, Repos, Commits
-from unittest.mock import Mock, patch
-from nose.tools import assert_is_not_none, assert_list_equal
+from unittest.mock import Mock, MagicMock
+import requests
 
-class test_commits(unittest.TestCase):
-    @Mock('requests.get')
-    def test_Commits(self, mockedReq):
-        mockedReq.return_value = True
-        commits = Commits(self.username, self.repo_name)
-        self.assert_Equal(commits, True)
+def github_repos_commits(username):
+    repos = Repos(username)
+    output = {}
+    count = 0
+    for repo in repos:
+        count = count + 1
+        repo_name = repo["name"]
+        output[repo_name] = Commits(username, repo_name)
+    print("Total number of repos for this user:", count)
+    return output
 
+
+# create function to find repos
+def Repos(username):
+    r = requests.get("https://api.github.com/users/" + username + "/repos")
+    data = r.json()
+    return data
+
+
+# create function to find number of commits per repo
+def Commits(username, repo_name):
+    r = requests.get("https://api.github.com/repos/" + username + "/" + repo_name + "/commits")
+    data = r.json()
+    print("Number of commits for",repo_name,":",len(data))
+    return len(data)
+
+@Mock('requests.get')
+def test_Commits(self, mockedReq):
+    mockedReq.return_value = MagicMock('MockSuccess')
+    commits = Commits(self.username, self.repo_name)
+    self.assertEqual(commits, 'MockSuccess')
